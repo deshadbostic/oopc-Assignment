7dfb933e6d6d31559869ac5d6d2528c3ac332f07
De Shad Bostic <deshadbostic@gmail.com>
2020-11-24 08:02:45 -0800

    Update Zhen.java
    changed zhen

--- src/Zhen.java
+++ src/Zhen.java
@@ -7,62 +7,95 @@ public class Zhen extends GamePiece implements ControlActions
         type = "zhen";
         symbol= 'z';
     }//Zhen constructor 
-    public int findNewPos(int direction)
+    public int[] findNewPos(int direction)
     {
-        System.out.println("Please enter a number to move: \n 1 to move north \n 2 to move south \n 3 to move east \n 4 to move west \n 5 to move north-west \n 6 to move south-west \n 7 to move south-east \n 8 to move north-east");
-        direction = input.nextInt();
+       int row=rowPos;
+       int col=colPos;
         if(direction == 1)
         {
-            colPos = colPos - 1;
-            return direction;
+            row = row + 1;
         }
         else if(direction == 2)
         {
-            colPos = colPos + 1;
-            return direction;
+            row = row - 1;
         }
         else if (direction == 3)
         {
-            rowPos = rowPos + 1;
-            return direction;
+            col = col + 1;
         }
         else if (direction == 4)
         {
+            col = col - 1;
+        }
+        else if (direction == 5)
+        {
+            col = col - 1;
+            row = row + 1;
+            
+        }
+        else if (direction == 6)
+        {
+            col = col - 1;
+            row = row - 1;
+            
+        }
+        else if (direction == 7)
+        {
+            col = col + 1;
+            row = row - 1;
+            
+        }
+        else if (direction == 8)
+        {
+            col = col + 1;
+            row = row + 1;
+            
+        }
+        return [row,col];
+    }
+
+    public void moveToNewPos(int direction)
+    {
+        if(direction == 1)
+        {
+            rowPos = rowPos + 1;
+        }
+        else if(direction == 2)
+        {
             rowPos = rowPos - 1;
-            return direction;
+        }
+        else if (direction == 3)
+        {
+            colPos = colPos + 1;
+        }
+        else if (direction == 4)
+        {
+            colPos = colPos - 1;
         }
         else if (direction == 5)
         {
             colPos = colPos - 1;
-            rowPos = rowPos - 1;
-            return direction;
+            rowPos = rowPos + 1;
+            
         }
         else if (direction == 6)
         {
-            colPos = colPos + 1;
+            colPos = colPos - 1;
             rowPos = rowPos - 1;
-            return direction;
+            
         }
         else if (direction == 7)
         {
             colPos = colPos + 1;
-            rowPos = rowPos + 1;
-            return direction;
+            rowPos = rowPos - 1;
+            
         }
         else if (direction == 8)
         {
-            colPos = colPos - 1;
+            colPos = colPos + 1;
             rowPos = rowPos + 1;
-            return direction;
+            
         }
-        return 0;
-    }
-    public boolean checkNewPos(int row,int col)
-    {
-        return true;
-    }
-    public void moveToNewPos(int direction)
-    {
         
     }
 }//Zhen class
